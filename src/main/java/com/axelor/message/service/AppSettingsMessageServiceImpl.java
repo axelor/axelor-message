d12f841076a948bb1913157d057a0db9b61fe616
Lawrence DENEIPLE <l.deneiple@axelor.com>
2023-03-17 10:40:47 +0100

    Make AppSettingsMessageServiceImpl singleton

--- src/main/java/com/axelor/message/service/AppSettingsMessageServiceImpl.java
+++ src/main/java/com/axelor/message/service/AppSettingsMessageServiceImpl.java
@@ -2,7 +2,11 @@ package com.axelor.message.service;
 
 import com.axelor.app.AppSettings;
 import com.axelor.utils.service.AppSettingsServiceImpl;
+import com.google.inject.Singleton;
+import javax.annotation.concurrent.ThreadSafe;
 
+@Singleton
+@ThreadSafe
 public class AppSettingsMessageServiceImpl extends AppSettingsServiceImpl implements
     AppSettingsMessageService {
 
