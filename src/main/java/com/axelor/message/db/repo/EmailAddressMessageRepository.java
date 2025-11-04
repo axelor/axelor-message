7b9b783a4be53c27489f793927a64dda397b6ab3
Ayoub bakraoui <a.bakraoui@axelor.com>
2025-11-04 12:37:46 +0100

    RM-103189: Rename EmailAddressRepo

--- src/main/java/com/axelor/message/db/repo/EmailAddressMessageRepository.java
+++ src/main/java/com/axelor/message/db/repo/EmailAddressMessageRepository.java
@@ -5,7 +5,7 @@ import com.axelor.message.db.EmailAddress;
 import com.axelor.message.exception.MessageExceptionMessage;
 import java.util.regex.Pattern;
 
-public class EmailAddressRepo extends EmailAddressRepository {
+public class EmailAddressMessageRepository extends EmailAddressRepository {
   private static final String EMAIL_PATTERN =
       "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
   private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
